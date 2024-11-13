b3b2f8942cd796538d7ccced226f3564d364e970
AshishKumarSah384 <aashishsah72@gmail.com>
2024-11-13 17:18:07 -0600

    adding code

---
+++ DayTwo.py
@@ -0,0 +1,15 @@
+if 5 < 2:
+    print("Five is greater than two")
+elif 5 > 2:
+    print("five is smaller than two")
+else:
+    print("shashnk")
+x = "3"
+y = 3
+z = 3.0
+
+b = float(x)
+c = float(y)
+d = float(z)
+a = b+c+d
+print (a)
--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,4 @@
 print("Hello world")
 print("My name is Ashish")
+print("Sujal")
+
