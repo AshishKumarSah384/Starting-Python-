ffb6c7bb614a85eed05a416815c2990f452b4d52
AshishKumarSah384 <aashishsah72@gmail.com>
2024-11-14 22:25:22 -0600

    Create Dayfour.py
    list insert and append

---
+++ learning/Dayfour/Dayfour.py
@@ -0,0 +1,14 @@
+ashishlist = ["apple" , "banana", "cherry"]
+print(ashishlist)
+
+fruitlists = ["mango" ,"grapes" , "orange"]
+print(fruitlists)
+
+
+fruitlists = ["apple" , "banana", "cherry" ,"orange", "mango"]
+print(fruitlists[0])
+print(fruitlists[-1])
+fruitlists[2] = "blueberry"
+print(fruitlists)
+fruitlists.insert(2,"blueberry")
+print(fruitlists)
